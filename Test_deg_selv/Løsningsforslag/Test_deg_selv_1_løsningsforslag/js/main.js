bb566c2743394b3c2b6b08f1a406cf7fb44199b3
Mads Opstad Reistadbakk <mareis@MBP.local>
2020-09-03 17:44:23 +0200

    ,

---
+++ Test_deg_selv/Løsningsforslag/Test_deg_selv_1_løsningsforslag/js/main.js
@@ -0,0 +1,24 @@
+//------------------- Oppgave 2.1 ----------------------
+
+//------------------- Oppgave 2.2 ----------------------
+
+//------------------- Oppgave 2.3 ----------------------
+
+//------------------- Oppgave 3.1 ----------------------
+
+let etg1 = 2;
+let type1 = "K";
+let romNr1 = 5;
+
+let etg2 = 1;
+let type2 = "M";
+let romNr2 = 9;
+
+let etg3 = 5;
+let type3 = "L";
+let romNr3 = 14;
+
+const oppgave3_1 = document.querySelector("#oppgave3_1");
+oppgave3_1.innerHTML = `<li>${etg1}${type1}${romNr1}</li>`;
+oppgave3_1.innerHTML += `<li>${etg2}${type2}${romNr2}</li>`;
+oppgave3_1.innerHTML += `<li>${etg3}${type3}${romNr3}</li>`;
