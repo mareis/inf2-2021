1af92b47abf6a0741ba6d78dead6f292cc3a6498
mareis <madsor@gmail.com>
2021-01-15 12:07:45 +0100

    ,

--- Snake/game.js
+++ Snake/game.js
@@ -1,5 +1,6 @@
 let lastRenderTime = 0
 const SNAKE_SPEED = 2
+const gameBoard = document.querySelector("#game-board");
 function main(currentTime) {
     window.requestAnimationFrame(main);
     const secondsSinceLastRenderTime = (currentTime - lastRenderTime) / 1000
@@ -18,6 +19,14 @@ function update() {
     console.log("update")
 }
 
+const snakeBody = [{ x: 11, y: 11 }]
 function draw() {
-    console.log("draw ")
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = segment.x;
+        snakeElement.style.gridColumnStart = segment.y;
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement);
+
+    })
 }
